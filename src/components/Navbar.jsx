efacfe01904b39d6cd2dac38eca116afaa363844
V102002 <vssnsvs@gmail.com>
2025-07-10 19:16:33 +0530

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { navLink } from '../../constants'
+import { useGSAP } from '@gsap/react'
+
+const Navbar = () => {
+    useGSAP(()=>{
+        const navTween=gsap.timeline({
+            scrollTrigger:{
+                trigger:'nav',
+                start:'bottom top' //When the bottom of the nav hits the top of viewport
+
+            }
+        });
+
+        navTween.fromTo('nav',{backgroundColor:'transparent'} ,{
+            backgroundColor:'#00000050',
+            backgroundFilter:'blur(10)',
+            duration:1,
+            ease:'power1.inOut'
+        });
+    })
+  return (
+    <nav>
+        <div>
+            <a href="#home" className='flex items-center gap-2'>
+                <img src="/images/logo.png" alt="logo" />
+                <p>Velvet Pour</p>
+            </a>
+            <ul>
+                {navLink.map((link)=>(
+                    <li key={link.id}>
+                        <a href={`#${link.id}`}>{link.title}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
